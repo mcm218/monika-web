7b1e08d8621f0d375deb66a73b7dd3ba6246a1fc
Michael Muniz <michael.muniz@scifusion360.com>
2020-03-11 10:38:38 -0400

    update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,16 +14,28 @@ export const helloWorld = functions.region("us-central1").https.onRequest((reque
 
 export const getUserToken = functions.region("us-central1").https.onRequest((request, response) => {
   return cors()(request, response, () => {
-    console.log("Logging user id...");
-    console.log(request.body.data.userId);
+    console.log("Creating token...");
     if (request.body.data.userId) {
-      admin.auth().createCustomToken(request.body.data.userId).then((customToken) => {
-        response.status(200).send({ data: { customToken: customToken } });
+      // mark user as a bot
+      if (request.body.data.bot) {
+        console.log("User is a bot...");
+        admin.auth().createCustomToken(request.body.data.userId, { bot: true }).then((customToken) => {
+          response.status(200).send({ data: { customToken: customToken } });
+        }
+        ).catch(error => {
+          console.log(error);
+          response.status(400).send({ error: { status_code: 400, msg: error } });
+        });
+
+      } else {
+        admin.auth().createCustomToken(request.body.data.userId).then((customToken) => {
+          response.status(200).send({ data: { customToken: customToken } });
+        }
+        ).catch(error => {
+          console.log(error);
+          response.status(400).send({ error: { status_code: 400, msg: error } });
+        });
       }
-      ).catch(error => {
-        console.log(error);
-        response.status(400).send({ error: { status_code: 400, msg: error } });
-      });
     } else {
       response.status(400).send({ error: { status_code: 400, msg: "Invalid body: id is required" } });
     }
